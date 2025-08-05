b4d0de047718072135521416c9e8b9cc18fb765b
DevSaloni <168973005+DevSaloni@users.noreply.github.com>
2025-08-05 19:57:18 +0530

    Update Signup.jsx

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -23,7 +23,7 @@ const Signup = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await fetch("http://localhost:2011/api/auth/signup", {
+      const res = await fetch("https://smartchat-history-manager.onrender.com/api/auth/signup", {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData)

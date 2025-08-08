5572018f2db5a6376915461c22010d901f165dbf
DevSaloni <168973005+DevSaloni@users.noreply.github.com>
2025-08-08 10:15:34 +0530

    Update Chat.js

--- server/models/Chat.js
+++ server/models/Chat.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const chatSchema = new mongoose.Schema({
-  userId: { type: String, required: true },
+userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   title: { type: String, default: 'Untitled Chat' },
   messages: [
     {
